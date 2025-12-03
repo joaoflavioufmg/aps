0493e0539b2933048bb05b51f7091e782ff50dcc
joaoflavioufmg <joao.flavio@dep.ufmg.br>
2025-12-03 00:29:20 -0300

    APS .dat ajustado

--- otimiza_glpk.bat
+++ otimiza_glpk.bat
@@ -3,4 +3,4 @@ echo Running GLPSOL ...
 REM call glpsol -m aps.mod -d aps.dat -d 31.dat --mipgap 0.001 --cuts
 @REM call glpsol -m aps.mod -d aps.dat --mipgap 0.05 --cuts
 @REM call glpsol -m aps.mod -d aps.dat --mipgap 0.01 --cuts --scale --adv --check
-call glpsol -m aps.mod -d aps.dat --mipgap 0.02 --cuts --scale --adv 
+call glpsol -m aps.mod -d aps.dat --mipgap 0.01 --cuts --scale --adv 
