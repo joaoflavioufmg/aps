1c04fcada223cfdbcdbf7ef100b0176c7d0057d1
joaoflavioufmg <joao.flavio@dep.ufmg.br>
2025-12-04 23:19:10 -0300

    Aps exemplo 5 com relatório melhor para base de dados

---
+++ models-bkp/numerical-example-5/otimiza_glpk.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Running GLPSOL ...
+REM call glpsol -m aps.mod -d aps.dat -d 31.dat --mipgap 0.001 --cuts
+@REM call glpsol -m aps.mod -d aps.dat --mipgap 0.05 --cuts
+@REM call glpsol -m aps.mod -d aps.dat --mipgap 0.01 --cuts --scale --adv --check
+call glpsol -m aps.mod -d aps.dat --cuts --scale --adv --mipgap 1.e-2 
+@REM call glpsol -m aps.mod -d LS.dat -d LS_distdur.dat --mipgap 0.01 --cuts --scale --adv 
